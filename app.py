0135756ed03ce89a911a6489051398d52d52d686
unknown <shashankperso@gmail.com>
2021-03-08 10:33:08 +0530

    implemented cart system + multi meme support : 5th march

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, session, redirect, send_file
 import requests
 import urllib.request
+import webbrowser
 
 resp = requests.get('https://api.imgflip.com/get_memes')
 if resp.status_code != 200:
@@ -8,7 +9,6 @@ if resp.status_code != 200:
 
 resp = resp.json()
 data = resp["data"]
-print(resp)
 cnt = 0
 memes = dict()
 username = "bobbyboibobby"
@@ -23,14 +23,20 @@ for item in data["memes"]:
 
 print(memes.values())
 app = Flask(__name__)
+app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         details = request.form
-        selected_img = details["meme"]
-        text0 = details["text0"]
-        text1 = details["text1"]
+        selected_img = details.getlist("meme")
+        print(selected_img)
+        session["selected_img"] = selected_img
+        #text0 = details["text0"]
+        #text1 = details["text1"]
+        return redirect(url_for('checkout'))
+
+
         new_meme = {"template_id":selected_img,"username": username,"password":password,"text0":text0,"text1":text1}
         resp = requests.post('https://api.imgflip.com/caption_image',params=new_meme)
         if resp.json()['success'] != True:
@@ -42,6 +48,82 @@ def home():
 
 
 
+@app.route('/checkout', methods=['GET', 'POST'])
+def checkout():
+    selected_img = session["selected_img"]
+    selected_URL = [memes[k] for k in selected_img]
+    if request.method == 'POST':
+        for memeid in selected_img:
+            if str(memeid)+" rem" in request.form:
+                ind = selected_img.index(memeid)
+                selected_img.remove(memeid)
+                selected_URL.remove(selected_URL[ind])
+                session["selected_img"] = selected_img
+                return redirect(url_for('checkout'))
+
+        texts=[]
+        details = request.form
+        for i in range(len(selected_img)):
+
+            t0 = details[str(selected_img[i])+" 0"]
+            t1 = details[str(selected_img[i])+" 1"]
+            texts.append([selected_img[i],t0,t1])
+        session["selected_texts"] = texts
+        return redirect(url_for('preview'))
+
+    
+    return render_template('checkout.html',numOfMemes = len(selected_img),memeID=selected_img,memeU=selected_URL)
+
+
+@app.route('/preview', methods=['GET', 'POST'])
+def preview():
+    selected_img = session["selected_img"]
+    selected_URL = [memes[k] for k in selected_img]
+    selected_texts = session["selected_texts"]
+    print(selected_texts)
+    gen_urls=[]
+    for i in range(len(selected_img)):
+        new_meme = {"template_id":selected_img[i],"username": username,"password":password,"text0":selected_texts[i][1],"text1":selected_texts[i][2]}
+        resp = requests.post('https://api.imgflip.com/caption_image',params=new_meme)
+        if resp.json()['success'] != True:
+            return resp.json()['error_message']
+        else:
+            gen_urls.append(resp.json()['data']['url'])
+    
+
+
+    if request.method == 'POST':
+        for memeid in selected_img:
+            if str(memeid)+" rem" in request.form:
+                ind = selected_img.index(memeid)
+                selected_img.remove(memeid)
+                gen_urls.remove(gen_urls[ind])
+                session["selected_img"] = selected_img
+                return redirect(url_for('preview'))
+            if str(memeid)+" edit" in request.form:
+                details = request.form
+                ind = selected_img.index(memeid)
+                t0 = details[str(memeid)+" 0"]
+                t1 = details[str(memeid)+" 1"]
+                selected_texts[ind] = [memeid,t0,t1]
+                session["selected_texts"] = selected_texts
+                return redirect(url_for('preview'))
+            
+        
+            
+
+
+        texts=[]
+        details = request.form
+        for i in range(len(selected_img)):
+
+            t0 = details[str(selected_img[i])+" 0"]
+            t1 = details[str(selected_img[i])+" 1"]
+            texts.append([selected_img[i],t0,t1])
+        session["selected_texts"] = texts
+        return redirect(url_for('preview'))
+    return render_template('preview.html',numOfMemes = len(selected_img),memeID=selected_img,memeU=gen_urls)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
